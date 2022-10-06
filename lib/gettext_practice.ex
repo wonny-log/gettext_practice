ea4f1ab0e2493ae4e213d8bc21246dc31e25f835
wonny-log <wonny727@gmail.com>
2022-10-06 11:35:12 +0900

    Add gettexts/0

--- lib/gettext_practice.ex
+++ lib/gettext_practice.ex
@@ -1,18 +1,52 @@
 defmodule GettextPractice do
-  @moduledoc """
-  Documentation for `GettextPractice`.
-  """
+  import GettextPractice.Gettext
 
-  @doc """
-  Hello world.
+  def gettexts do
+    # Simple translation
+    # => result: "번역할 문자열"
+    gettext("번역할 문자열")
+    |> IO.inspect()
 
-  ## Examples
+    # Plural translation
+    ngettext(
+      "세 번째 인수가 1인 경우 이 문자열을 번역함",
+      "세 번째 인수가 1이 아닌 경우 이 문자열을 번역함",
+      1
+    )
+    |> IO.inspect()
 
-      iex> GettextPractice.hello()
-      :world
+    # => result: "세 번째 인수가 1이 아닌 경우 이 문자열을 번역함"
+    ngettext(
+      "세 번째 인수가 1인 경우 이 문자열을 번역함",
+      "세 번째 인수가 1이 아닌 경우 이 문자열을 번역함",
+      2
+    )
+    |> IO.inspect()
 
-  """
-  def hello do
-    :world
+    ngettext(
+      "세 번째 인수가 1인 경우 이 문자열을 번역함",
+      "세 번째 인수가 0인 경우에는 이 문자열을 번역함",
+      0
+    )
+    |> IO.inspect()
+
+    # Domain-based translation
+    # 첫 번째 인수에 들어온 값과 동일한 이름을 가진 PO 파일 내용으로 번역됨
+    dgettext("errors", "번역할 에러 메시지")
+    |> IO.inspect()
+
+    # Context-based translation
+    pgettext("email", "번역할 문자열")
+    |> IO.inspect()
+
+    # All of the above
+    dpngettext(
+      "errors",
+      "context",
+      "세 번째 인수가 1인 경우 이 문자열을 번역함",
+      "세 번째 인수가 1이 아닌 경우 이 문자열을 번역함",
+      2
+    )
+    |> IO.inspect()
   end
 end
