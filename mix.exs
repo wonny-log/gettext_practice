9eb83442e6ca5336a7d104760eafa045a4bddb84
wonny-log <wonny727@gmail.com>
2022-10-06 11:05:34 +0900

    Install gettext

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule GettextPractice.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:gettext, "~> 0.20.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
